599dcccf783928edc80c62d951415549a029f870
snljty <snljty@gmail.com>
2026-03-28 11:06:59 +0800

    add moments of inertia type detection

--- src/MolSymm_point_group.cpp
+++ src/MolSymm_point_group.cpp
@@ -1,6 +1,7 @@
 #include "MolSymm.hpp"
 
 std::string Molecule::detect_point_group(double tol) const {
+    // quick return
     if (natoms == 1) return "Kh";
     if (natoms == 2) return elements[0] == elements[1] ? "Dinfh" : "Cinfv";
 
@@ -8,6 +9,7 @@ std::string Molecule::detect_point_group(double tol) const {
 
     Eigen::Matrix3d moments_of_inertia_tensor(Eigen::Matrix3d::Zero());
 
+    // get moments of inertia
     for (int iatom = 0; iatom < natoms; ++ iatom) {
         moments_of_inertia_tensor += atomic_weights[iatom] * (
             coords_centerized.col(iatom).transpose() * coords_centerized.col(iatom) * Eigen::Matrix3d::Identity() 
@@ -18,6 +20,7 @@ std::string Molecule::detect_point_group(double tol) const {
     Eigen::Vector3d moments_of_inertia = eigen_solver.eigenvalues();
     // Eigen::Matrix3d principal_axes = eigen_solver.eigenvectors();
 
+    // detect SEA
     Eigen::MatrixXi atomic_numbers_to_compare = atomic_numbers.replicate(1, natoms);
 
     Eigen::MatrixXd distance_to_compare;
@@ -30,14 +33,15 @@ std::string Molecule::detect_point_group(double tol) const {
         distance_to_compare = diff.colwise().norm().reshaped(natoms, natoms);
     }
 
+    // argsort !
     Eigen::VectorXd sort_method(natoms);
     for (int iatom = 0; iatom < natoms; ++ iatom) {
         std::iota(sort_method.data(), sort_method.data() + natoms, 0);
         std::sort(sort_method.data(), sort_method.data() + natoms,
             [iatom, &distance_to_compare](int i, int j) 
             { return distance_to_compare(i, iatom) < distance_to_compare(j, iatom); });
-        distance_to_compare.col(iatom) = distance_to_compare.col(iatom)(sort_method).eval();
-        atomic_numbers_to_compare.col(iatom) = atomic_numbers_to_compare.col(iatom)(sort_method).eval();
+        distance_to_compare.col(iatom) = distance_to_compare.col(iatom)(sort_method).eval(); // must be a .eval() or there is aliasing issue.
+        atomic_numbers_to_compare.col(iatom) = atomic_numbers_to_compare.col(iatom)(sort_method).eval(); // must be a .eval() or there is aliasing issue.
     }
 
     std::vector<bool> touched(natoms, false);
@@ -55,6 +59,20 @@ std::string Molecule::detect_point_group(double tol) const {
             touched[jatom] = true;
             SEAs.back().push_back(jatom);
         }
+    }
+
+    if (moments_of_inertia[0] <= tol && moments_of_inertia[2] - moments_of_inertia[1] <= tol) {
+        // linear, I_A = 0, I_B = I_C
+        fmt::print("{:s}\n", "linear");
+    } else if (moments_of_inertia[1] - moments_of_inertia[0] <= tol && moments_of_inertia[2] - moments_of_inertia[1] <= tol) {
+        // more than one main-axes where n > 2, I_A = I_B = I_C, a.k.a. "spherial-like"
+        fmt::print("{:s}\n", "spherial-like");
+    } else if (moments_of_inertia[1] - moments_of_inertia[0] <= tol || moments_of_inertia[2] - moments_of_inertia[1] <= tol) {
+        // symmetric, I_A = I_B \ne I_C or I_A \ne I_B = I_C
+        fmt::print("{:s}\n", "symmetric");
+    } else {
+        // asymmetric, I_A \ne I_B \ne I_C
+        fmt::print("{:s}\n", "asymmetric");
     }
 
     /*
