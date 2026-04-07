8c833e4669f5cc95891401ccd78c2c27f7fbb3dc
snljty <snljty@gmail.com>
2026-04-07 16:02:45 +0800

    add command argument to set tolerance support

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "MolSymm.hpp"
 
 int main(int argc, const char *argv[]) {
-    if (argc - 1 != 1) throw std::invalid_argument(fmt::format("Usage: {:s} xxx.xyz", argv[0]));
-    Molecule mol(argv[1]);
-    fmt::print("{:s}\n", mol.detect_point_group());
+    if (argc - 1 != 1 && argc -1 != 2) throw std::invalid_argument(fmt::format("Usage: {:s} xxx.xyz", argv[0]));
+    std::string molname(argv[1]);
+    Molecule mol(molname);
+    std::string result;
+    if (argc - 1 == 2) {
+        double tol = std::stod(std::string(argv[2]));
+        result = mol.detect_point_group(tol);
+    } else {
+        result = mol.detect_point_group();
+    }
+    fmt::print("{:s}\n", result);
     // mol.use_new_coordinates();
     // mol.write_gjf("new.gjf");
 
